5bf5efbcdc32cf79bc624e0044f5740c98cfe3a2
Brian Vu <brianvu7@gmail.com>
2022-03-14 17:11:13 -0500

    commit tree 4441341e31ded70ba1e6b15703b515b4ea9d9690

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
 
+    def name
+        @this_dogs_name
+    end
 end
+
+snoopy = Dog.new
+snoopy.name = "Snoopy"
+puts snoopy.name
